e1368630c74c22e912fb099d9254eece92dd5993
Bhavik2209 <bhavikrohit22@gmail.com>
2024-08-25 23:17:25 +0530

    last try (for tab icon only)

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import manage_tips, toggle_reaction, get_tips
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', manage_tips, name='manage_tips'),
     path('toggle_reaction/<str:tip_id>/<str:reaction_type>/', toggle_reaction, name='toggle_reaction'),
     path('get-tips/<str:section>/', get_tips, name='get_tips'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
